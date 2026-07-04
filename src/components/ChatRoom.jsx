7a4d08ed5eb1414457fd626af383980e9ebc3240
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-04 07:59:59 +0530

    Update ChatRoom.jsx

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,6 +1,55 @@
+import { useEffect, useRef, useState } from "react";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot
+} from "firebase/firestore";
+
+import { db } from "../firebase";
+
+import Message from "./Message";
 import SendMessage from "./SendMessage";
 
 function ChatRoom({ room }) {
+
+  const [messages, setMessages] = useState([]);
+
+  const bottomRef = useRef();
+
+  useEffect(() => {
+
+    const q = query(
+      collection(db, "rooms", room, "messages"),
+      orderBy("createdAt")
+    );
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+
+      const list = [];
+
+      snapshot.forEach((doc) => {
+
+        list.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+
+      });
+
+      setMessages(list);
+
+      setTimeout(() => {
+        bottomRef.current?.scrollIntoView({
+          behavior: "smooth",
+        });
+      }, 100);
+
+    });
+
+    return () => unsubscribe();
+
+  }, [room]);
 
   return (
 
@@ -10,7 +59,14 @@ function ChatRoom({ room }) {
 
       <div className="messages">
 
-        <p>Realtime messages will appear here.</p>
+        {messages.map((msg) => (
+          <Message
+            key={msg.id}
+            message={msg}
+          />
+        ))}
+
+        <div ref={bottomRef}></div>
 
       </div>
 
@@ -19,6 +75,7 @@ function ChatRoom({ room }) {
     </div>
 
   );
+
 }
 
 export default ChatRoom;
