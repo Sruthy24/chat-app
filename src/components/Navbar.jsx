e1d473ebc15db35c5dac607dcdaf8b5e6c34ce96
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-04 07:37:28 +0530

    Add Navbar component with logout functionality

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+
+function Navbar() {
+
+  return (
+    <nav className="navbar">
+
+      <h2>Firebase Chat</h2>
+
+      <button onClick={() => signOut(auth)}>
+        Logout
+      </button>
+
+    </nav>
+  );
+}
+
+export default Navbar;
