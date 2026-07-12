5ecaad31ddaa09ec798cc5453aad421bd7b90928
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-12 20:07:05 +0530

    Fix export statement in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,13 @@ import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 
 function Navbar() {
-
   return (
     <nav className="navbar">
-
       <h2>Firebase Chat</h2>
 
       <button onClick={() => signOut(auth)}>
         Logout
       </button>
-
     </nav>
   );
 }
