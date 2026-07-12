39277fdcc3b1aadfbddc25f048252a28fb501f77
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-12 19:50:04 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,21 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyDmob29uwAN1Oke4gBD9yvBxavwihx8wVg",
+
   authDomain: "react-firebase-chat-1d338.firebaseapp.com",
+
   projectId: "react-firebase-chat-1d338",
+
   storageBucket: "react-firebase-chat-1d338.firebasestorage.app",
+
   messagingSenderId: "702279492350",
+
   appId: "1:702279492350:web:c27ee2ab3f3b36247a3baf",
+
   measurementId: "G-TNRW43CM9S"
+
 };
 
 const app = initializeApp(firebaseConfig);
