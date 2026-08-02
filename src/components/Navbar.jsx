b7dd9eb0d464aa5fdda4db4794704f68d82848bf
Sruthy24 <sruthysmnglm@gmail.com>
2026-08-02 17:22:20 +0530

    Update Navbar with user details and logout functionality
    Enhance Navbar to display user profile and online status.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,37 @@ import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 
 function Navbar() {
+  const user = auth.currentUser;
+
   return (
     <nav className="navbar">
-      <h2>Firebase Chat</h2>
 
-      <button onClick={() => signOut(auth)}>
-        Logout
-      </button>
+      <div className="nav-logo">
+        💬 ConnectHub
+      </div>
+
+      <div className="nav-user">
+
+        <img
+          src={user?.photoURL}
+          alt="profile"
+          className="profile-image"
+        />
+
+        <div className="user-details">
+          <h4>{user?.displayName}</h4>
+          <span>🟢 Online</span>
+        </div>
+
+        <button
+          className="logout-btn"
+          onClick={() => signOut(auth)}
+        >
+          Logout
+        </button>
+
+      </div>
+
     </nav>
   );
 }
