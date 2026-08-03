f3ca50a3cc6aa3e77352fe35a5a47c61cee7cf4b
Sruthy24 <sruthysmnglm@gmail.com>
2026-08-03 21:16:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import "./App.css";
 import { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import { auth } from "./firebase";
+
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 import RoomList from "./components/RoomList";
 import ChatRoom from "./components/ChatRoom";
 
 function App() {
-  const [selectedRoom, setSelectedRoom] = useState("General");
+  const [user] = useAuthState(auth);
+  const [selectedRoom, setSelectedRoom] = useState("");
 
+  // Show Login Page
+  if (!user) {
+    return <Login />;
+  }
+
+  // Show Chat App
   return (
     <div className="app">
+      <Navbar />
 
-      {/* Sidebar */}
-      <div className="sidebar">
-        <RoomList
-          selectedRoom={selectedRoom}
-          setSelectedRoom={setSelectedRoom}
-        />
+      <div className="main-container">
+        <div className="sidebar">
+          <RoomList
+            selectedRoom={selectedRoom}
+            setSelectedRoom={setSelectedRoom}
+          />
+        </div>
+
+        <div className="chat-container">
+          {selectedRoom ? (
+            <ChatRoom roomId={selectedRoom} />
+          ) : (
+            <div
+              style={{
+                flex: 1,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "24px",
+                color: "#666",
+              }}
+            >
+              💬 Select a room to start chatting
+            </div>
+          )}
+        </div>
       </div>
-
-      {/* Chat Area */}
-      <div className="chat-container">
-        <ChatRoom roomId={selectedRoom} />
-      </div>
-
     </div>
   );
 }
